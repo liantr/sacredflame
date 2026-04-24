599abb95d8c28bcc2a82f86a23f54b8f672e320f
Lian Randle <liantrandle@gmail.com>
2026-04-23 19:04:41 -0700

    reorganize

--- src/Player.lua
+++
@@ -1,8 +0,0 @@
-Player = Class{__includes=Entity}
-
-function Player:init(def, world, startX, startY)
-    Entity.init(self, def, world, startX, startY)
-    self.fixture:setUserData({type='player'})
-    self.canJump = true
-    self.runSpeed = def.runSpeed
-end
