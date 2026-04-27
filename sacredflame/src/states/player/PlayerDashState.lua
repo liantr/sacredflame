ffa23a464bf2e02c1b427bfb3ef2c78405ab4cd2
Lian Randle <liantrandle@gmail.com>
2026-04-27 16:42:34 -0700

    new run state + changing sprite

---
+++ sacredflame/src/states/player/PlayerDashState.lua
@@ -0,0 +1,15 @@
+PlayerDashState = Class{__includes = BaseState}
+
+function PlayerDashState:init(player)
+    self.entity = player
+end
+
+function PlayerDashState:enter(params)
+end
+
+function PlayerDashState:update(dt)
+end
+
+function PlayerDashState:render()
+end
+
