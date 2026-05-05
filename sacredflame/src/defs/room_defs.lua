df73ce9424ff1f7b9ec9bbf383abd44c0707ef6f
Lian Randle <liantrandle@gmail.com>
2026-05-05 16:46:20 -0700

    cleanup torch placement in room 2

--- sacredflame/src/defs/room_defs.lua
+++ sacredflame/src/defs/room_defs.lua
@@ -88,7 +88,7 @@ ROOM_DEFS= {
             {
                 type = 'torch',
                 spawnX = TILE_SIZE * 20,
-                spawnY = TILE_SIZE * 4
+                spawnY = TILE_SIZE * 5.5
             },
             {
                 type = 'wall-hold',
@@ -98,103 +98,4 @@ ROOM_DEFS= {
         },
         enemies = {}
     },
-    -- ['main3'] = {
-    --     map = 'assets/graphics/map/main3.lua',
-    --     connectedRooms = {
-    --         north = { room = 'main2', spawnX = 16*6, spawnY = VIRTUAL_HEIGHT - TILE_SIZE * 3},
-    --         south = { room = 'main4', spawnX = TILE_SIZE*80 - TILE_SIZE*3, spawnY = TILE_SIZE},
-    --         east = nil,
-    --         west = nil
-    --     }
-    -- },
-    -- ['main4'] = {
-    --     map = 'assets/graphics/map/main4.lua',
-    --     connectedRooms = {
-    --         north = nil,
-    --         south = nil,
-    --         east = nil,
-    --         west = nil
-    --     }
-    -- },
-    -- ['main5'] = {
-    --     map = 'assets/graphics/map/main5.lua',
-    --     connectedRooms = {
-    --         north = nil,
-    --         south = nil,
-    --         east = nil,
-    --         west = nil
-    --     }
-    -- },
-    -- ['main1-right'] = {
-    --     map = 'assets/graphics/map/main1-right.lua',
-    --     connectedRooms = {
-    --         north = nil,
-    --         south = nil,
-    --         east = nil,
-    --         west = { room = 'main1', spawnX = VIRTUAL_WIDTH - TILE_SIZE, spawnY = TILE_SIZE*6}
-    --     }
-    -- },
-    -- ['main2-left'] = {
-    --     map = 'assets/graphics/map/main2-left.lua',
-    --     connectedRooms = {
-    --         north = nil,
-    --         south = nil,
-    --         east = { room = 'main2', spawnX = TILE_SIZE, spawnY = TILE_SIZE * 8},
-    --         west = nil
-    --     }
-    -- },
-    -- ['main3-left'] = {
-    --     map = 'assets/graphics/map/main3-left.lua',
-    --     connectedRooms = {
-    --         north = nil,
-    --         south = nil,
-    --         east = nil,
-    --         west = nil
-    --     }
-    -- },
-    -- ['main3-right'] = {
-    --     map = 'assets/graphics/map/main3-right.lua',
-    --     connectedRooms = {
-    --         north = nil,
-    --         south = nil,
-    --         east = nil,
-    --         west = nil
-    --     }
-    -- },
-    -- ['main4-right'] = {
-    --     map = 'assets/graphics/map/main4-right.lua',
-    --     connectedRooms = {
-    --         north = nil,
-    --         south = nil,
-    --         east = nil,
-    --         west = nil
-    --     }
-    -- },
-    -- ['main5-right'] = {
-    --     map = 'assets/graphics/map/main5-right.lua',
-    --     connectedRooms = {
-    --         north = nil,
-    --         south = nil,
-    --         east = nil,
-    --         west = nil
-    --     }
-    -- },
-    -- ['main5-left'] = {
-    --     map = 'assets/graphics/map/main5-left.lua',
-    --     connectedRooms = {
-    --         north = nil,
-    --         south = nil,
-    --         east = nil,
-    --         west = nil
-    --     }
-    -- },
-    -- ['boss-room'] = {
-    --     map = 'assets/graphics/map/boss.lua',
-    --     connectedRooms = {
-    --         north = nil,
-    --         south = nil,
-    --         east = nil,
-    --         west = nil
-    --     }
-    -- },
 }
